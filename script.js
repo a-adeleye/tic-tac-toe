0c5ddeb33f017d94fc9a29523edbfd789206dac1
aadeleye <adeleyeadeyemib@gmail.com>
2022-01-05 16:15:49 +0100

    html and css setup

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let tiles = document.querySelectorAll(".tile");
 
 tiles.forEach((tile) => tile.addEventListener("click", markTile));
 
+
+
 function markTile() {
   if (this.textContent) return;
   this.textContent = "X";
+  this.style.color = '#171614';
   console.log("tile");
 }
