057dc0a80d88f88af705d6cdc8b019238cb2cf63
aadeleye <adeleyeadeyemib@gmail.com>
2022-01-06 14:08:13 +0100

    updated with computer versus

--- script.js
+++ script.js
@@ -14,26 +14,30 @@ const winningConditions = [
   [2, 4, 6],
 ];
 
-const gameOver = document.querySelector('.endGame');
-const restart = document.querySelector('#restart');
+const gameOver = document.querySelector(".endGame");
+const restart = document.querySelector("#restart");
 
 const gamePlay = (() => {
-  let currentPlayer = o;
-  let winner = '';
+  let currentPlayer = x;
+  let winner = "";
   const dark = "#171614";
   const light = "#eddfef";
+  let color = dark;
 
-  const changeTurn = () => {
-    currentPlayer === x
-      ? ((currentPlayer = o), (color = light))
-      : ((currentPlayer = x), (color = dark));
-  };
+  const changePlayer = () => {
+    currentPlayer = x;
+    color = dark;
+  }
+
+  const changeComp = () => {
+    currentPlayer = o;
+    color = light;
+  }
 
   const updateTurn = () => {
     document.querySelector(
       ".turn"
     ).textContent = `Player ${currentPlayer}'s turn`;
-    gamePlay.changeTurn();
   };
 
   const markBoard = (e) => {
@@ -51,68 +55,113 @@ const gamePlay = (() => {
   };
 
   const checkWinner = () => {
-      for (let i = 0; i <= 7; i++) {
-        const winCondition = winningConditions[i];
-        const a = gameBoard[winCondition[0]];
-        const b = gameBoard[winCondition[1]];
-        const c = gameBoard[winCondition[2]];
-        if (!a || !b || !c) {
-          continue;
-        } else {
-          if (a === b && b === c) {
-            winner = currentPlayer;
-            console.log(winner);
-            break;
-          }
+    for (let i = 0; i <= 7; i++) {
+      const winCondition = winningConditions[i];
+      const a = gameBoard[winCondition[0]];
+      const b = gameBoard[winCondition[1]];
+      const c = gameBoard[winCondition[2]];
+      if (!a || !b || !c) {
+        continue;
+      } else {
+        if (a === b && b === c) {
+          winner = currentPlayer;
+          console.log(winner);
+          break;
         }
       }
+    }
   };
 
-const endGame = () => {
-    let winningMessage = document.createElement('p');
-    winningMessage.classList.add('winner');
-    winningMessage.textContent = `${currentPlayer} wins!`
-    if(winner){
-        gameOver.insertBefore(winningMessage,restart);
-        gameOver.classList.add('show');
-    } if(!winner && gameBoard.length == 9 && !gameBoard.includes("")){
-        winningMessage.textContent = `It's a tie`
-        gameOver.insertBefore(winningMessage,restart);
-        gameOver.classList.add('show');
+  const endGame = () => {
+    let winningMessage = document.createElement("p");
+    winningMessage.classList.add("winner");
+    winningMessage.textContent = `${currentPlayer} wins!`;
+    if (winner) {
+      gameOver.insertBefore(winningMessage, restart);
+      gameOver.classList.add("show");
     }
-}
+    if (!winner && gameBoard.length == 9 && !gameBoard.includes("")) {
+      winningMessage.textContent = `It's a tie`;
+      gameOver.insertBefore(winningMessage, restart);
+      gameOver.classList.add("show");
+    }
+  };
 
-function reset(elem){
-    elem.style.cursor = 'pointer';
-    elem.textContent = '';
-}
+  function reset(elem) {
+    elem.style.cursor = "pointer";
+    elem.textContent = "";
+  }
 
   const restartGame = () => {
-    let el = document.querySelector('.winner');
+    let el = document.querySelector(".winner");
     gameBoard = ["", "", "", "", "", "", "", "", ""];
-    tiles.forEach(tile => reset(tile));
-    gameOver.classList.replace('show', 'hide');
+    tiles.forEach((tile) => reset(tile));
+    gameOver.classList.replace("show", "hide");
     winner = false;
-    tiles.forEach((tile) => tile.addEventListener("click", play, { once: true }));
+    tiles.forEach((tile) =>
+      tile.addEventListener("click", play, { once: true })
+    );
     el.parentNode.removeChild(el);
+    currentPlayer = x;
+    color = dark;
   };
 
-  return { changeTurn, updateTurn, markBoard, updateGameBoard, checkWinner, endGame, restartGame };
+  const computerMove = () => {
+    let emptyTiles = [];
+    let tileIndex = "";
+    if (gameBoard.includes("")) {
+      for (i = 0; i < gameBoard.length; i++) {
+        if (gameBoard[i] == "") {
+          emptyTiles.push(i);
+        }
+      }
+      tileIndex = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+      gameBoard[tileIndex] = currentPlayer;
+      tiles[tileIndex].style.cssText = `cursor: not-allowed; color: ${color}`;
+      tiles[tileIndex].textContent = currentPlayer;
+      tiles[tileIndex].removeEventListener("click", play);
+    }
+  };
+
+  const roundEnd = () => {
+    if (winner) return true;
+    else return false;
+  };
+
+  return {
+    markBoard,
+    updateGameBoard,
+    changePlayer,
+    changeComp,
+    updateTurn,
+    restartGame,
+    computerMove,
+    checkWinner,
+    endGame,
+    roundEnd
+  };
 })();
 
 const play = () => {
-  gamePlay.updateTurn();
   gamePlay.markBoard();
   gamePlay.updateGameBoard();
+  //gamePlay.updateTurn();
   gamePlay.checkWinner();
   gamePlay.endGame();
+  gamePlay.changeComp();
+  if (!gamePlay.roundEnd()) {
+    gamePlay.computerMove();
+    gamePlay.checkWinner();
+    gamePlay.endGame();
+    gamePlay.changePlayer();
+  }
 };
 
 let tiles = Array.from(document.querySelectorAll(".tile"));
 
 document.addEventListener("DOMContentLoaded", () => {
   tiles.forEach((tile) => tile.addEventListener("click", play, { once: true }));
-  restart.addEventListener('click', gamePlay.restartGame);
+  restart.addEventListener("click", gamePlay.restartGame);
 });
 
 /* let playerOne = Math.floor(Math.random() * 3) < 2;
